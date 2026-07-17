2203fa6e5041f4656097296e665040cc863da0d0
AJ Royce Niran George <135869002+aroycenirangeorge@users.noreply.github.com>
2026-07-17 19:40:32 +0530

    Refactor line buffer and pixel processing logic

--- src/hls.cpp
+++ src/hls.cpp
@@ -8,44 +8,58 @@ void isp_hls_top(hls::stream<pkt_t> &s_axis, hls::stream<pkt_t> &m_axis, int gai
     #pragma HLS INTERFACE axis port=m_axis
     #pragma HLS INTERFACE s_axilite port=gain bundle=CTRL
     #pragma HLS INTERFACE s_axilite port=return bundle=CTRL
-    static unsigned char line_buf[3][1280];
+
+    static unsigned char line_buf[2][1280];
     #pragma HLS ARRAY_RESHAPE variable=line_buf complete dimension=1
+
     static unsigned char window[3][3];
     #pragma HLS ARRAY_PARTITION variable=window complete dimension=0
+
     for (int y = 0; y < 720; y++) {
         for (int x = 0; x < 1280; x++) {
             #pragma HLS PIPELINE II=1
             pkt_t curr_pkt;
-            s_axis.read(curr_pkt); 
-            line_buf[2][x] = line_buf[1][x];
-            line_buf[1][x] = line_buf[0][x];
+            s_axis.read(curr_pkt);
             unsigned int gained = (curr_pkt.data & 0xFF) * gain;
-            line_buf[0][x] = (gained > 4080) ? 255 : (gained >> 4);
+            unsigned char new_pixel = (gained > 4080) ? 255 : (gained >> 4);
+
             for(int i = 0; i < 3; i++) {
                 window[i][2] = window[i][1];
                 window[i][1] = window[i][0];
-                window[i][0] = line_buf[i][x];
             }
-            pkt_t out_pkt = curr_pkt; 
-            unsigned char R, G, B;
-            unsigned char p11 = window[0][0], p12 = window[0][1], p13 = window[0][2];
-            unsigned char p21 = window[1][0], p22 = window[1][1], p23 = window[1][2];
-            unsigned char p31 = window[2][0], p32 = window[2][1], p33 = window[2][2];
+
+            window[0][0] = new_pixel;
+            window[1][0] = line_buf[0][x];
+            window[2][0] = line_buf[1][x];
+
+            line_buf[1][x] = line_buf[0][x];
+            line_buf[0][x] = new_pixel;
+
+            unsigned char p11 = window[2][2], p12 = window[2][1], p13 = window[2][0];
+            unsigned char p21 = window[1][2], p22 = window[1][1], p23 = window[1][0];
+            unsigned char p31 = window[0][2], p32 = window[0][1], p33 = window[0][0];
+
+            pkt_t out_pkt;
+            unsigned char R = 0, G = 0, B = 0;
+
             if (y < 2 || x < 2) {
                 out_pkt.data = 0;
             } else {
-                if (y % 2 == 0) { 
-                    if (x % 2 == 0) { 
+                int img_y = y - 1;
+                int img_x = x - 1;
+
+                if (img_y % 2 == 0) {
+                    if (img_x % 2 == 0) {
                         R = p22;
                         G = (p12 + p21 + p23 + p32) >> 2;
                         B = (p11 + p13 + p31 + p33) >> 2;
-                    } else { 
+                    } else {
                         R = (p21 + p23) >> 1;
                         G = p22;
                         B = (p12 + p32) >> 1;
                     }
                 } else {
-                    if (x % 2 == 0) { 
+                    if (img_x % 2 == 0) {
                         R = (p12 + p32) >> 1;
                         G = p22;
                         B = (p21 + p23) >> 1;
@@ -57,6 +71,14 @@ void isp_hls_top(hls::stream<pkt_t> &s_axis, hls::stream<pkt_t> &m_axis, int gai
                 }
                 out_pkt.data = (R << 16) | (G << 8) | B;
             }
+
+            out_pkt.keep = curr_pkt.keep;
+            out_pkt.strb = curr_pkt.strb;
+            out_pkt.id   = curr_pkt.id;
+            out_pkt.dest = curr_pkt.dest;
+
+            out_pkt.user = (x == 0 && y == 0) ? 1 : 0;
+            out_pkt.last = (x == 1279) ? 1 : 0;
 
             m_axis.write(out_pkt);
         }
